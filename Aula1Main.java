cb01b08e2c166a11b991504537fa44196c64f75f
Guilherme_Esber <168978954+MelzitoZz@users.noreply.github.com>
2025-08-18 16:18:38 -0300

    Update Aula1Main.java

--- Aula1Main.java
+++ Aula1Main.java
@@ -1,56 +1,26 @@
 package br.edu.unifei.ecot12.prova;
 
-public class Avaliacao {
-    public static final float NOTA_MINIMA=6.0f;
-    private long matricula;
-    private float nota1;
-    private float nota2;
-    private float frequencia;
-    private float substitutiva;
-    public float media(){
-        float nota=(nota1+nota2)/2;
-        if(nota<NOTA_MINIMA){
-            if(nota1<nota2){
-                nota=(substitutiva+nota2)/2;
-            }else{
-                nota=(substitutiva+nota1)/2;
-            }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Avaliacao a = new Avaliacao();
+        Scanner s = new Scanner(System.in);
+        System.out.println("matricula");
+        a.setMatricula(s.nextLong());
+        System.out.println("nota1");
+        a.setNota1(s.nextFloat());
+        a.setMatricula(s.nextLong());
+        System.out.println("nota2");
+        a.setNota2(s.nextFloat());
+        if(a.media()<Avaliacao.NOTA_MINIMA){
+            System.out.println("substitutiva");
+            a.setSubstitutiva(0);
         }
-        return nota;
-    }
-
-    public boolean aprovacao(){
-        return frequencia>=0.75 && media()>=NOTA_MINIMA;
-    }
-
-    public long getMatricula() {
-        return matricula;
-    }
-    public void setMatricula(long matricula) {
-        this.matricula = matricula;
-    }
-    public float getNota1() {
-        return nota1;
-    }
-    public void setNota1(float nota1) {
-        this.nota1 = nota1;
-    }
-    public float getNota2() {
-        return nota2;
-    }
-    public void setNota2(float nota2) {
-        this.nota2 = nota2;
-    }
-    public float getFrequencia() {
-        return frequencia;
-    }
-    public void setFrequencia(float frequencia) {
-        this.frequencia = frequencia;
-    }
-    public float getSubstitutiva() {
-        return substitutiva;
-    }
-    public void setSubstitutiva(float substitutiva) {
-        this.substitutiva = substitutiva;
+        System.out.println("frequencia");
+        a.setFrequencia(s.nextFloat());
+        System.out.println("media:"+a.media());
+        System.out.println(a.aprovacao());
+        s.close();
     }
 }
